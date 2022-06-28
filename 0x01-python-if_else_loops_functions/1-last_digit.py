0be293df5d6ec860736c83073befd5ddc5f1df2b
kingsleyabonyi <kinabonyi@gmail.com>
2022-06-28 13:24:50 +0000

    random radient

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number < 0:
+    remainder = number % -10
+else:
+    remainder = number % 10
+print('Last digit of', number, 'is', number)
 if number > 5:
     print('and is greater than 5')
 elif number == 0:
