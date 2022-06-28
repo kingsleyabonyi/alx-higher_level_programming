64e8a874017afcd6a0ebe94e7e758732f01fd446
kingsleyabonyi <kinabonyi@gmail.com>
2022-06-28 13:35:55 +0000

    random radient

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ if number < 0:
     remainder = number % -10
 else:
     remainder = number % 10
-print('Last digit of', number, 'is', number)
-if number > 5:
+print('Last digit of', number, 'is', remainder, end=' ')
+if remainder > 5:
     print('and is greater than 5')
-elif number == 0:
+elif remainder == 0:
     ('and is zero')
 else:
     print('and is less than 6 and 0')
