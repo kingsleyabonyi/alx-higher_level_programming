19086a7f5eaca52b3d83f3b43db699eefe22f9d5
kingsleyabonyi <kinabonyi@gmail.com>
2022-08-03 00:06:23 +0000

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Implements the class_to_json function
+"""
+
+
+def class_to_json(obj):
+    """
+        Returns the dict representation of a class
+    """
+    return (getattr(obj, "__dict__"))
