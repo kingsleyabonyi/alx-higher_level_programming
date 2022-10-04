5cbca18aec5e7c4e5a49227de301e4a866c6a218
kingsleyabonyi <kinabonyi@gmail.com>
2022-10-04 15:18:05 +0000

    loop languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i in args) {
   console.log(args[i]);
 }
