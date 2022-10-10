c61d03a937f6e4e9810c1811311a773f01cda8e8
kingsleyabonyi <kinabonyi@gmail.com>
2022-10-10 04:21:18 +0000

    model task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a python file that contains the class definition of a City and
+    an instance Base = declarative_base()
+"""
+
+from sys import argv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """A class that inherits from the decarative base"""
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
