78c1295edc790bcbbc736631eaa7f995d78c734f
kingsleyabonyi <kinabonyi@gmail.com>
2022-10-10 05:48:45 +0000

    task 15

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,24 @@
+ble file 23 lines (19 sloc) 738 Bytes
+#!/usr/bin/python3
+""" a script that lists all City objects from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State, Base
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='California')
+    new_state.cities = [City(name='San Francisco')]
+    session.add(new_state)
+    session.commit()
+    session.close()
