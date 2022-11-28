bb66b1875c4f78f7739729a82678980f872977a4
kingsleyabonyi <kinabonyi@gmail.com>
2022-11-28 03:18:52 +0100

    to create username and all privilages

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Creates the user with all privilages.
-CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON ',' To 'user_0d_1'@'localhost;
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost;
