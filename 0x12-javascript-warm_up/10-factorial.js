89f38b3ef0fc0a31a629504a031141a38b25bced
kingsleyabonyi <kinabonyi@gmail.com>
2023-01-20 18:17:10 +0100

    js warm-up

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,6 @@
-
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+const argument = process.argv[2] * 1;
+function factorial (fac) {
+  return ((isNaN(fac)) || (fac === 1) ? 1 : (fac * factorial(fac - 1)));
 }
-
-console.log(factorial(Number(process.argv[2])));
+console.log(factorial(argument));
