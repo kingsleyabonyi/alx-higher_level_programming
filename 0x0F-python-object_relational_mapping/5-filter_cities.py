e7331951fda92475181fae63829699fe7e1ad5be
kingsleyabonyi <kinabonyi@gmail.com>
2023-01-23 18:13:57 +0100

    filter_city

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""A module that list all data in `state` table in `hbtn_0e_0_usa` database
-    with name starting with 'N'
- """
-
-
+'''database connection script'''
 from sys import argv
 import MySQLdb
 
-
+'''connection Setup'''
 if __name__ == '__main__':
-    dataBaseConnect = MySQLdb.connect(host='localhost',
-                                      port=3306, user=argv[1],
-                                      passwd=argv[2], db=argv[3],
-                                      charset='utf8')
-    data = dataBaseConnect.cursor()
-    data.execute("SELECT cities.name FROM cities WHERE cities.state_id = \
-                (SELECT state.id FROM states WHERE states.name = '{}' \
-                 LIMIT 1) ORDER BY cities.id ASC".format(argv[4]))
-    dataTable = data.fetchall()
-    newData = []
-    for eachRow in dataTable:
-        newData.append(eachRow[0])
-    print(", ".join(newData))
-    data.close()
-    dataBaseConnect.close()
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    db = connection.cursor()
+    db.execute("SELECT cities.name FROM cities" +
+               " INNER JOIN states ON cities.state_id = states.id" +
+               " WHERE states.name LIKE %s;", [argv[4]])
+    query_rows = db.fetchall()
+    print(', '.join(map(lambda x: x[0], query_rows)))
+
+    db.close()
+    connection.close()
